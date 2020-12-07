7fb1deed7585edbad097c9027eb4e283b59f18bb
jiangwei1 <jiangwei1@kylinos.com>
2020-12-07 10:33:06 +0800

    增加wsgi启动文件

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+"""
+# coding:utf-8
+@Time    : 2020/12/07
+@Author  : jiangwei
+@mail    : jiangwei1@kylinos.cn
+@File    : wsgi.py
+@Software: PyCharm
+"""
+from dnf import create_app
+
+app = create_app()
